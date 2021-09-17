a4696a96868f16381db1c5b26cc094c9a75b12d2
Isaac <isaac2010411@gmail.com>
2021-09-17 13:03:02 -0300

    fix home data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import React , {useEffect , useState} from "react";
+import React  from "react";
 import "./index.css";
-import { getAllService } from "./services";
 
 function App() {
-   const [is , setIs] = useState(null)
-  useEffect(()=>{
-    async function get (){
-      await getAllService('/').then(d=>{ setIs(d)})
-    }
-    if(!is){
-      get()
-    }
 
-  },[is])
   return <div className=''>
       <h1>Esto es app</h1>
   </div>;
