9b4adcbfb7ed30007bfb5e02f23bdfc7e93389e7
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-09-18 01:42:30 +0000

    Estructura del Footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import LoaderSpinner from "./LoaderSpinner";
 import Alert from "./Alert";
+import Footer from "./Footer";
 
-export { 
-    LoaderSpinner,
-    Alert
-};
+export { LoaderSpinner, Alert, Footer };
