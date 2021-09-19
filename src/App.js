8797a88db4ccf8fc9a6a7afa88856a5c1ad5ce6a
Octavio Peralta <octi.pro20@gmail.com>
2021-09-19 11:44:27 -0300

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import LoginForm from "./components/LoginForm";
 import "./index.css";
 
 function App() {
   return (
     <div className=''>
-      <LoginForm />
-      {/* <h1>Esto es app</h1> */}
+      <h1>Esto es app</h1>
     </div>
   );
 }
