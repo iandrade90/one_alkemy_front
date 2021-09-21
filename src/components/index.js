93935cf7332f512faa0395d166528eeeda9901ed
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-09-21 11:25:30 +0000

    Creación de formulario de registro

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,19 @@
+import Alert from "./Alert";
+import Information from "./Profile/Info";
+import FormEditSlide from "./Slider/FormEditSlide";
+import EditNameOrganization from "./Organization/EditNameOrganization";
 import LoaderSpinner from "./LoaderSpinner";
 import BackofficeNavigation from "./BackOficeNavigation";
 import Footer from "./Footer";
-import Alert from "./Alert";
-import Information from './Profile/Info';
-import FormEditSlide from "./Slider/FormEditSlide";
-import EditNameOrganization from './Organization/EditNameOrganization';
-
+import RegisterForm from "./RegisterForm";
 
 export {
-    FormEditSlide,
-    EditNameOrganization,
-    LoaderSpinner,
-    Information,
-    Alert,
-    BackofficeNavigation,
-    Footer
+  FormEditSlide,
+  EditNameOrganization,
+  LoaderSpinner,
+  Information,
+  Alert,
+  BackofficeNavigation,
+  Footer,
+  RegisterForm,
 };
