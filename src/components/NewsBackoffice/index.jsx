fcbe1250b6934a0469fa73582f41f6c40778a4a4
acostamatias <matuu64@gmail.com>
2021-09-29 21:05:43 -0300

    Fix

--- src/components/NewsBackoffice/index.jsx
+++ src/components/NewsBackoffice/index.jsx
@@ -1,60 +1,40 @@
 import React from 'react';
-import {BsPencil, BsTrash} from "../../../icons/index";
-require('./index.css')
 
 const NewsBackoffice = () => {
-    const news = [
-        {
-            name: 'Test',
-            image: 'http://psicoterapeutas.eu/imagenes-psicoterapeutas-eu/tests-objetivos.png',
-            createdAt: '13-09-2021'
-        },
-        {
-            name: 'Test',
-            image: 'http://psicoterapeutas.eu/imagenes-psicoterapeutas-eu/tests-objetivos.png',
-            createdAt: '13-09-2021'
-        },
-        {
-            name: 'Test',
-            image: 'http://psicoterapeutas.eu/imagenes-psicoterapeutas-eu/tests-objetivos.png',
-            createdAt: '13-09-2021'
-        },
-        {
-            name: 'Test',
-            image: 'http://psicoterapeutas.eu/imagenes-psicoterapeutas-eu/tests-objetivos.png',
-            createdAt: '13-09-2021'
-        }
-    ]
     return(
-        <div className="card shadow-sm m-3 p-2">
-        <table class="table table-hover news-table my-3 text-center">
-            <thead>
-                <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Imagen</th>
-                <th scope="col">Fecha</th>
-                <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                {news.map((item) => (
-                    <tr>
-                        <td>{item.name}</td>
-                        <td>
-                            <div>
-                                <img src={item.image} width="125" alt={item.name} />
-                            </div>
-                        </td>
-                        <td>{item.createdAt}</td>
-                        <td>
-                            <button className="btn btn-primary me-1"><BsPencil /></button>
-                            <button className="btn btn-danger"><BsTrash /></button>
-                        </td>
-                    </tr>
-                ))} 
-            </tbody>
-        </table>
-        </div>
+        <>
+            <section className="border-bottom">
+                <div className="table-responsive">
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                            <th scope="col">Título</th>
+                            <th scope="col">Imagen</th>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {news.map((item) => (
+                                <tr>
+                                    <td>{item.name}</td>
+                                    <td>
+                                        <div>
+                                            <img src={item.image} width="125" height="auto" alt={item.name} />
+                                        </div>
+                                    </td>
+                                    <td>{item.createdAt}</td>
+                                    <td>
+                                        <Modal modalconfig={deleteModal(item)} />
+                                        <Modal modalconfig={modifyModal(item)} />
+                                    </td>
+                                </tr>
+                            ))} 
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </>
     )
 }
 
