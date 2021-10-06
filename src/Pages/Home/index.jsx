0c8b1960def0fc03cfdc844ce0d065e22c1c0df7
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-10-06 16:07:32 -0300

    clear test constant

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
-    const a = getAllService("/news").then((res) => {
+    getAllService("/news").then((res) => {
       setNewsData(res.data);
     });
   }, []);
