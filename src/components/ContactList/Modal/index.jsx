7e32efda532930c93626688dfd6b0b3f352fc5bb
acostamatias <matuu64@gmail.com>
2021-10-11 23:03:11 -0300

    Delete contact

---
+++ src/components/ContactList/Modal/index.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import Backdrop from "../../Activities/Backdrop";
+import { motion } from "framer-motion";
+import "./style.css";
+import {
+  HiOutlineExclamation,
+  HiX,
+} from "../../../icons";
+
+const dropIn = {
+  hidden: {
+    y: "-20vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      type: "spring",
+      damping: 32,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "-20vh",
+    opacity: 0,
+  },
+};
+
+const Modal = ({ handleClose, data, onSubmit }) => {
+
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="modal-sms"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+                {console.log(data)}
+
+        <DeleteModal data={data} onSubmit={onSubmit} />
+        <button className="close-button" onClick={handleClose}>
+          <HiX />
+        </button>
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+export default Modal;
+
+const DeleteModal = ({ onSubmit, data }) => {
+  return (
+    <>
+      <div className="modal-sms-body">
+        <div className="modal-icon delete">
+          <HiOutlineExclamation />
+        </div>
+        <div>
+          <h4>Eliminar contacto</h4>
+          <p>
+            Estas seguro que deseas eliminar el contacto{" "}
+            <strong>{data.item.name}</strong> ? Toda la información relacionada sera
+            eliminada permanentemente. Esta acción no puede deshacerse.
+          </p>
+        </div>
+        {console.log(data)}
+      </div>
+      <div className="modal-sms-footer">
+        <button
+          className="btn btn-delete shadow-sm"
+          onClick={() => onSubmit({ data, type: "delete" })}
+        >
+          Eliminar
+        </button>
+        <button className="btn btn-cancel shadow-sm">Cancelar</button>
+      </div>
+    </>
+  );
+};
--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,39 +1,52 @@
 // import axios from 'axios';
 import React, {useEffect,useState} from 'react';
-import { getAllService } from '../../services';
+import { BsTrash } from "../../icons/index";
+import { AnimatePresence } from "framer-motion";
+import {
+    deleteService,
+    getAllService,
+} from "../../services";
+import Modal from "./Modal";
+
 
 const ContactList = () => {
-    const [data, setData] = useState([]);
-    // const endpoint = "http://localhost:3001/api/v1/contacts";
-    // const getData = async (endpoint) => {
-    //     return await axios.get(endpoint);
-    // }
-    // useEffect(() => {
-    //     const result = getData(endpoint);
-    //     setData(result.data);
-    // }, [])
+    const [contactList, setContactList] = useState([]);
+    const [newContactData, setNewContactData] = useState({});
+    const [modal, setModal] = useState(false);
+
+    const close = () => {
+        setNewContactData({});
+        setModal(false);
+    };
+    
+    const open = (data) => {
+        setNewContactData(data);
+        setModal(true);
+    };
 
     useEffect(() => {
         getAllService("contacts")
         .then((res) => {
-            setData(res.data);
+            setContactList(res.data);
         })
         .catch((error)=>{
           console.log(error)
         })
       }, []);
 
-    const example = [{
-        name: 'Test',
-        phone: '12345678',
-        email: 'email@email.com',
-        message: 'test'
-    }]
+    const handleSubmit = async (payload) => {
+        let contactList;
+        await deleteService(`contacts/${payload.data.id}`);
+        const newContactList = contactList.filter((contact) => contact.id !== payload.data.id);
+        setContactList(newContactList);
+        close();
+    };
+
     return (
         <div className="container">
             <div className="card table-responsive table-striped m-3 p-2">
                 {
-                    data.message ? (<h5>{data.message}</h5>)
+                    contactList.message ? (<h5>{contactList.message}</h5>)
                     :
                     <table className="table table-hover my-3 ">
                     <thead>
@@ -41,23 +54,41 @@ const ContactList = () => {
                         <th scope="col">Nombre</th>
                         <th scope="col">Email</th>
                         <th scope="col">Mensaje</th>
+                        <th scope="col">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data.length>0 && data.map((item) => (
+                        {contactList.length>0 && contactList.map((item) => (
                             <tr key={item.id}>
                                 <td>{item.name}</td>
                                 <td>{item.email}</td>
                                 <td>{item.message}</td>
+                                {console.log(item)}
+                                <td>
+                                    <button
+                                    className="btn btn-lg btn-danger"
+                                    onClick={() => open({ item, delete: true })}
+                                    >
+                                        <BsTrash />
+                                    </button>
+                                </td>
                             </tr>
                         ))
-                    
                     } 
                     </tbody>
                 </table>
                 }
-                
             </div>
+            <AnimatePresence inital={false} exitBeforeEnter={true}>
+                {modal && (
+                <Modal
+                    modal={modal}
+                    data={newContactData}
+                    handleClose={close}
+                    onSubmit={handleSubmit}
+                />
+                )}
+            </AnimatePresence>
         </div>
     )
 }
